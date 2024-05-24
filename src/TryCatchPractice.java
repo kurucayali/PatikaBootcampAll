7ef61539cdc900764200465b199f3e2673a6ece7
Ali Kurucay <kurucayali@gmail.com>
2024-05-24 12:00:26 +0300

    new

--- src/TryCatchPractice.java
+++ src/TryCatchPractice.java
@@ -7,15 +7,15 @@ public class TryCatchPractice {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Dizinin görmek istediğiniz indeks numarasını giriniz : ");
         try {
             int indexNum = input.nextInt();
-            int result= arr[indexNum];
-            System.out.println("Dizinin "+indexNum+". indeksindeki eleman : "+ result);
-        }catch (ArrayIndexOutOfBoundsException e){
+            int result = arr[indexNum];
+            System.out.println("Dizinin " + indexNum + ". indeksindeki eleman : " + result);
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Hata: indeks dizi boyutunun dışında ! ");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("HATA ! :  " + e.toString());
         }
 
