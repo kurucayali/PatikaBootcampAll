8da5bbff92ca89058934b455583989132dea6ffa
Ali Kurucay <kurucayali@gmail.com>
2024-06-12 01:01:21 +0300

    new

---
+++ DosyadanSayilariOkuTopla/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String path = "C:\\Users\\kurucay\\IdeaProjects\\PatikaBootcampAll\\DosyadanSayilariOkuTopla\\src\\Veri.txt";
+
+        File file = new File(path);
+
+        FileReader fReader = new FileReader(file);
+        BufferedReader bReader = new BufferedReader(fReader);
+        String line;
+        int total = 0;
+
+        while ((line = bReader.readLine()) != null) {
+            System.out.println(line);
+            int num = Integer.valueOf(line);
+            total = total + num;
+        }
+        System.out.println("Bu sayıların toplamı : " + total);
+    }
+}
